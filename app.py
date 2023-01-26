0b51f5ff4150dbcb657c36c2b1fb2e13f3b8a02f
“shedp” <sheenadiep@gmail.com>
2023-01-26 10:28:48 +0000

    defined books routes

--- app.py
+++ app.py
@@ -9,6 +9,25 @@ CORS(app)
 def welcome():
     return "Hello World", 200
 
+@app.route("/books", method=['GET','POST'])
+def books_index():
+    fns = {
+        'GET': books.index,
+        'POST': books.create
+    }
+    resp, code = fns[request.method](request)
+    return jsonify(resp), code
+
+@app.route('books/<int:book_id>', methods=['GET', 'PATCH','DELETE'])
+def books_show(book_id):
+    fns = {
+        'GET': books.show,
+        'PATCH': books.update,
+        'DELETE': books.destroy
+    }
+    resp, code = fns[request.method](request, book_id)
+    return jsonify(resp), code
+
 
 
 if __name__ == "__main__":
