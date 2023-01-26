da9cdcbab5eeea1cdde477c0862ebd827ed8d75f
“shedp” <sheenadiep@gmail.com>
2023-01-26 12:33:49 +0000

    imported controllers and debugs errors

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from werkzeug import exceptions
+from controllers import books
+from controllers import authors
 
-app = FLASK(__name__)
+
+app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def welcome():
-    return "Hello World", 200
+    return render_template('home.html')
 
-@app.route("/books", method=['GET','POST'])
+@app.route("/books", methods=['GET','POST'])
 def books_index():
     fns = {
         'GET': books.index,
@@ -18,7 +21,7 @@ def books_index():
     resp, code = fns[request.method](request)
     return jsonify(resp), code
 
-@app.route('books/<int:book_id>', methods=['GET', 'PATCH','DELETE'])
+@app.route('/books/<int:book_id>', methods=['GET', 'PATCH','DELETE'])
 def books_show(book_id):
     fns = {
         'GET': books.show,
@@ -28,21 +31,21 @@ def books_show(book_id):
     resp, code = fns[request.method](request, book_id)
     return jsonify(resp), code
 
-@app.route('authors', methods=['GET','POST'])
+@app.route('/authors', methods=['GET','POST'])
 def authors_index():
     fns = {
-        'GET': author.index,
-        'PATCH': author.create
+        'GET': authors.index,
+        'PATCH': authors.create
     }
-    res, code = fns[request.method](request)
+    resp, code = fns[request.method](request)
     return jsonify(resp), code
 
-@app.route('authors/<int:author_id>', methods=['GET', 'PATCH','DELETE'])
+@app.route('/authors/<int:author_id>', methods=['GET', 'PATCH','DELETE'])
 def author_show(author_id):
     fns = {
-        'GET': author.show,
-        'PATCH': author.update,
-        'DELETE': author.destroy
+        'GET': authors.show,
+        'PATCH': authors.update,
+        'DELETE': authors.destroy
     }
     resp, code = fns[request.method](request, author_id)
     return jsonify(resp), code
