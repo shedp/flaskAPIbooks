027487e3de828afdb9755db9fbb84b2a0628de55
“shedp” <sheenadiep@gmail.com>
2023-01-26 12:37:11 +0000

    moved data into sepearate folder

---
+++ data/authors.py
@@ -0,0 +1,7 @@
+authors = [
+    {'id': 1, 'author': 'Christopher St John Springg'},
+    {'id': 2, 'author': 'Ian Rankin'},
+    {'id': 3, 'author': 'Robin Le Poidevin'},
+    {'id': 4, 'author': 'Stephen Hawking'},
+    {'id': 5, 'author': 'John Le Carre'}
+]
---
+++ data/books.py
@@ -0,0 +1,11 @@
+books = [
+    {'id': 1, 'title': 'Death of an Airman', 'author': 'Christopher St John Springg', 'genre': 'crime', 'type': 'softback', 'status': 'read'},
+    {'id': 2, 'title': 'Knots & Crosses', 'author': 'Ian Rankin', 'genre': 'crime', 'type': 'softback', 'status': 'read'},
+    {'id': 3, 'title': 'Travels In Four Dimensions', 'author': 'Robin Le Poidevin', 'genre': 'science', 'type': 'softback', 'status': 'read'},
+    {'id': 4, 'title': 'A Brief History of Time', 'author': 'Stephen Hawking', 'genre': 'science', 'type': 'softback', 'status': 'unread'},
+    {'id': 5, 'title': 'The Spy Who Came from the Cold', 'author': 'John Le Carre', 'genre': 'crime', 'type': 'softback', 'status': 'read'},
+    {'id': 6, 'title': 'A Murder of Quality', 'author': 'John Le Carre', 'genre': 'crime', 'type': 'softback', 'status': 'unread'},
+    {'id': 7, 'title': 'The Looking Glass War', 'author': 'John Le Carre', 'genre': 'crime', 'type': 'softback', 'status': 'unread'},
+    {'id': 8, 'title': 'Call for the Dead', 'author': 'John Le Carre', 'genre': 'crime', 'type': 'softback', 'status': 'unread'},
+    {'id': 9, 'title': 'A Small Town in Germany', 'author': 'John Le Carre', 'genre': 'crime', 'type': 'softback', 'status': 'unread'}
+]
